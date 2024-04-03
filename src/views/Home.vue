418f4f1c6f5cceeea2fcc8999ec9dc85f8f04110
Tintswalo <110996874+calcaventia@users.noreply.github.com>
2024-04-03 13:53:10 +0200

    initial commit

---
+++ src/views/Home.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="home">
+    <h1 class="title">Welcome to Quick Invoice</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+  components: {},
+};
+</script>
